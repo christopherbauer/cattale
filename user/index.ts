99b21ff5d01ad9ff2e816b0772f3362da8641e8a
Christopher Bauer <chrisbauer.career@gmail.com>
2023-03-20 13:58:53 -0600

    Initialize the functional skeleton of the user service

---
+++ user/index.ts
@@ -0,0 +1,23 @@
+import express, { json } from "express";
+import { errorHandler, logger, requestLogHandler } from "core";
+const app = express();
+app.use(requestLogHandler);
+app.get("/", json(), (_request, response) => {
+	response.status(200).send({
+		live: true,
+	});
+});
+
+app.all("*", (_request, _result) => {
+	throw new Error("Route not found");
+});
+app.use(errorHandler);
+
+const start = () => {
+	const port = Number(process.env.PORT || 3000);
+	app.listen(port, () => {
+		logger.info(`Server running on port ${port}`);
+	});
+};
+
+start();
